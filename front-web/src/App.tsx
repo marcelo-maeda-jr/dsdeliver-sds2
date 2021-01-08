c672c7f0b2ddc97d76d4e5516fa63a1c372fecca
Marcelo Maeda Jr <marcelo.yoshi.maeda@gmail.com>
2021-01-08 20:41:40 -0300

    Corrigindo importações não utilizadas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
 import Routes from './Routes';
 
 function App() {
